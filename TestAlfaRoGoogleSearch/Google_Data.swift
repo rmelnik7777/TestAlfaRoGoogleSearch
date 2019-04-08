eb495e6e2611cf69d082588f7b1a1ff2bc294d61
Роман Мельник <rmelnik7777@gmail.com>
2019-04-08 21:12:46 +0300

    delete other mistake

--- TestAlfaRoGoogleSearch/Google_Data.swift
+++ TestAlfaRoGoogleSearch/Google_Data.swift
@@ -2,24 +2,10 @@
 //  Google_Data.swift
 //  TestAlfaRoGoogleSearch
 //
-//  Created by Viktor Drykin on 25.07.2018.
+//  Created by Roman Melnik  on 05.04.2019.
 //  Copyright © 2018 NGSE. All rights reserved.
 //
-
-import Foundation
-import SwiftyJSON
-
-class GoogleData: NSObject {
-    
+class GoogleData {
     var dataSearch: String = ""
     var searchURL: String = ""
-    
-    /*init?(json: JSON) {
-        guard let title = json["title"].string,
-            let link = json["link"].string else {
-                return nil
-        }
-        self.dataSearch = title
-        self.searchURL = link
-    }*/
 }
