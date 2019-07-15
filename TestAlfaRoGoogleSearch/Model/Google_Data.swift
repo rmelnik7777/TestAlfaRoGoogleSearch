80e0a016349d74a602c9327783dfce1f41ad9a47
Роман Мельник <rmelnik7777@gmail.com>
2019-07-15 16:28:34 +0300

    no message

---
+++ TestAlfaRoGoogleSearch/Model/Google_Data.swift
@@ -0,0 +1,11 @@
+//
+//  Google_Data.swift
+//  TestAlfaRoGoogleSearch
+//
+//  Created by Roman Melnik  on 05.04.2019.
+//  Copyright © 2018 NGSE. All rights reserved.
+//
+struct GoogleData {
+    var dataSearch: String = ""
+    var searchURL: String = ""
+}
---
+++ TestAlfaRoGoogleSearch/Services/Networking/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  TestAlfaRoGoogleSearch
+//
+//  Created by Роман Мельник on 7/1/19.
+//  Copyright © 2019 NGSE. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkManager {
+    
+}
